f696b0a0f4c1b4c0b00c231069433d86fa989d3b
Markus Olsson <olsson14@gmail.com>
2021-08-06 19:46:30 +0200

    Added new style of website.

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container about">
+    <div class="text-center p-3">
+      <h3 class="header">Who am I?</h3>
+      <p class="p-2">
+        I am a passionate developer with a constant strive to dig myself deeper
+        into the jungle of tech. I mostly do web development professionally and
+        I don't like to limit myself to any specific technology or side of the
+        stack. On my spare time I like to hit the gym, go on afterworks and
+        spend time with my girlfriend. If I get time to spare I tend to learn
+        new languages and frameworks, my newest passion is
+        <a href="https://www.rust-lang.org/">rust</a>.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="gradient app-header d-flex align-items-center">
+    <div class="container">
+      <div class="row d-flex align-items-center">
+        <div class="col-md-5 col-sm-12 mb-3">
+          <img
+            src="../assets/portrait.png"
+            class="rounded-circle mx-auto d-block profile-picture"
+            alt="Profile picture"
+          />
+        </div>
+        <div class="col-md-7 col-sm-12 text-center text-white">
+          <h2>Markus Olsson</h2>
+          <h3 class="py-2">Software Engineer</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+.app-header {
+  height: 400px;
+}
+
+.profile-picture {
+  max-width: 200px;
+  max-height: 400px;
+}
+</style>
