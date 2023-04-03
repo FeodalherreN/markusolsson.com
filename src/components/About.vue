73188be946c87437bcc1ddfa6cf6a7acf995ec68
Markus Olsson <markus.olsson@softhouse.se>
2023-04-03 22:33:40 +0200

    Updated description.

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,7 +7,7 @@
         into the jungle of tech. I mostly do web development professionally and
         I don't like to limit myself to any specific technology or side of the
         stack. On my spare time I like to hit the gym, go on afterworks and
-        spend time with my girlfriend. If I get time to spare I tend to learn
+        spend time with my friends. If I get time to spare I tend to learn
         new languages and frameworks, my newest passion is
         <a href="https://www.rust-lang.org/">rust</a>.
       </p>
